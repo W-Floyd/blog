e2ec63f407b24f58a541255e962e0a7f89afb9e5
William Floyd <william.png2000@gmail.com>
2020-03-13 06:58:59 +0100

    update: Prepare...

--- update.sh
+++ update.sh
@@ -2,8 +2,12 @@
 
 git pull
 
+if [ -d './static/images/generated/']; then
+    rm -r './static/images/generated/'
+fi
+
 find src -iname '*.sh' | while read -r __file; do
-    time ./${__file}
+    time ./${__file} "$(dirname "${__file}")/"
 done
 
 HUGO_ENV=production hugo --gc --minify
