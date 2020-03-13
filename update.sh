dc19d6a22113fa8d8861380c73040739b18013f4
William Floyd <william.png2000@gmail.com>
2020-03-13 07:44:06 +0100

    update: Update submodules as well.

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ if [ -d './static/images/generated/' ]; then
     rm -r './static/images/generated/'
 fi
 
+git submodule update --remote --merge
+
 find src -iname '*.sh' | while read -r __file; do
     time ./${__file} "$(dirname "${__file}")/"
 done
