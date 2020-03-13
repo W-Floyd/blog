7488bd8b323a30e617e80116babadb31dc211143
William Floyd <william.png2000@gmail.com>
2020-03-13 09:14:55 +0100

    src: Rename and add image copy.

---
+++ src/images_copy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+__target='./static/images/reduced/clickbait'
+__source='../blog-images/dirs/clickbait/reduced/'
+
+if [ -d "${__target}" ]; then
+    rm -r "${__target}"
+fi
+
+mkdir -p "${__target}"
+
+cp -r "${__source}" "${__target}"
+
+exit
