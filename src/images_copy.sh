9fb45c4f7dcf7a400db9862c47ef163c4f76fa98
William Floyd <william.png2000@gmail.com>
2020-05-06 19:55:10 -0400

    images_copy: Add midiMixer, use function.

--- src/images_copy.sh
+++ src/images_copy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-__target='./static/images/reduced/clickbait'
-__source='../blog-images/dirs/clickbait/reduced'
+__func() {
+    __target="${1}"
+    __source="${2}"
 
-if [ -d "${__target}" ]; then
-    rm -r "${__target}"
-fi
+    if [ -d "${__target}" ]; then
+        rm -r "${__target}"
+    fi
 
-mkdir -p "$(dirname "${__target}")"
+    mkdir -p "$(dirname "${__target}")"
 
-cp -r "${__source}" "${__target}"
+    cp -r "${__source}" "${__target}"
+
+}
+
+__func './static/images/reduced/clickbait' '../blog-images/dirs/clickbait/reduced'
+
+__func './static/images/reduced/midiMixer/v1' '../blog-images/dirs/dirs/midiMixer/v1/reduced'
 
 exit
