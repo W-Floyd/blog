0effef89f54ff7f9f24398e9bfa58572a08c21f1
William Floyd <william.png2000@gmail.com>
2020-05-06 23:26:20 -0400

    images_copy: Also copy originals.

--- src/images_copy.sh
+++ src/images_copy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-__func() {
+__target_prefix='./static/images'
+__source_prefix='../blog-images/dirs'
+
+__copy_reduced() {
+    __copy "${__target_prefix}/reduced/${1}/" "${__source_prefix}/${1}/reduced/"
+}
+
+__copy_original() {
+    __copy "${__target_prefix}/original/${1}/" "${__source_prefix}/${1}/"
+}
+
+__copy() {
     __target="${1}"
     __source="${2}"
 
@@ -10,14 +21,21 @@ __func() {
 
     mkdir -p "$(dirname "${__target}")"
 
-    cp -r "${__source}" "${__target}"
+    while read -r __file; do
+        cp -r "${__file}" "${__target}"
+    done < <(find "${__source}" -maxdepth 1 -type f -iname '*.jpg')
 
 }
 
-__func './static/images/reduced/clickbait' '../blog-images/dirs/clickbait/reduced'
+__func() {
+    __copy_reduced "${1}"
+    __copy_original "${1}"
+}
 
-__func './static/images/reduced/midiMixer/v1' '../blog-images/dirs/midiMixer/v1/reduced'
+__func 'clickbait'
 
-__func './static/images/reduced/midiMixer/handwired' '../blog-images/dirs/midiMixer/handwired/reduced'
+__func 'midiMixer/v1'
+
+__func 'midiMixer/handwired'
 
 exit
