9345d486a5b50227470d4efde929014c4ba350f8
William Floyd <william.png2000@gmail.com>
2022-03-17 02:33:50 -0500

    docker: Add pusher and dockerignore.

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo docker build -t w-floyd/blog . || {
+    echo 'Failure to build'
+    exit
+}
+
+sudo docker save w-floyd/blog | bzip2 | pv | ssh "${1}" docker load
+ssh "${1}" docker-compose -f /root/server-config/docker-compose.yml --project-directory /root/server-config up --remove-orphans -d
+
+exit
